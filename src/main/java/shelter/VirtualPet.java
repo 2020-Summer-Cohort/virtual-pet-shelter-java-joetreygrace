bab6828edb68e5923fda52b819acadae6563cc2b
Joe Grace <joeggrace@gmail.com>
2020-05-22 09:35:46 -0400

    Finished VirtualPet class, starting on VirtualPetShelter class

---
+++ src/main/java/shelter/VirtualPet.java
@@ -0,0 +1,67 @@
+package shelter;
+
+public class VirtualPet {
+    private String virtualPetName;
+    private String virtualPetDescriptor;
+    private int hungerLevel;
+    private int thirstLevel;
+    private int boredomLevel;
+
+    public VirtualPet(String virtualPetName, String virtualPetDescriptor) {
+        this.virtualPetName = virtualPetName;
+        this.virtualPetDescriptor = virtualPetDescriptor;
+        this.hungerLevel = 15;
+        this.thirstLevel = 15;
+        this.boredomLevel = 15;
+    }
+
+    public String getVirtualPetName() {
+        return virtualPetName;
+    }
+
+    public String getVirtualPetDescriptor() {
+        return virtualPetDescriptor;
+    }
+
+    public int getHungerLevel() {
+        return hungerLevel;
+    }
+
+    public int getThirstLevel() {
+        return thirstLevel;
+    }
+
+    public int getBoredomLevel() {
+        return boredomLevel;
+    }
+
+    public int giveWater(int thirstLevel) {
+        thirstLevel = thirstLevel - 15;
+        if (thirstLevel < 0) {
+            thirstLevel = 0;
+        }
+        return thirstLevel;
+    }
+
+    public int giveFood(int hungerLevel) {
+        hungerLevel = hungerLevel - 15;
+        if (hungerLevel < 0) {
+            hungerLevel = 0;
+        }
+        return hungerLevel;
+    }
+
+    public int playWithPet(int boredomLevel) {
+        boredomLevel = boredomLevel - 25;
+        if (boredomLevel < 0) {
+            boredomLevel = 0;
+        }
+        return boredomLevel;
+    }
+
+    public void tick() {
+        hungerLevel = hungerLevel + 5;
+        thirstLevel = thirstLevel + 5;
+        boredomLevel = boredomLevel + 5;
+    }
+}
--- src/test/java/shelter/VirtualPetTest.java
+++ src/test/java/shelter/VirtualPetTest.java
@@ -1,4 +1,73 @@
 package shelter;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VirtualPetTest {
+
+    @Test
+    public void shouldBeAbleToCreateNewVirtualPet() {
+        VirtualPet underTest = new VirtualPet("Sam", "Norwegian Elkhound");
+    }
+
+    @Test
+    public void shouldRetrieveHungerLevel() {
+        VirtualPet underTest = new VirtualPet("Sam", "Norwegian Elkhound");
+        assertEquals(15, underTest.getHungerLevel());
+    }
+
+    @Test
+    public void shouldRetrieveThirstLevel() {
+        VirtualPet underTest = new VirtualPet("Sam", "Norwegian Elkhound");
+        assertEquals(15, underTest.getThirstLevel());
+    }
+
+    @Test
+    public void shouldRetrieveBoredomLevel() {
+        VirtualPet underTest = new VirtualPet("Sam", "Norwegian Elkhound");
+        assertEquals(15, underTest.getBoredomLevel());
+    }
+
+    @Test
+    public void shouldRetrievePetName() {
+        VirtualPet underTest = new VirtualPet("Sam", "Norwegian Elkhound");
+        assertEquals("Sam", underTest.getVirtualPetName());
+    }
+
+    @Test
+    public void shouldRetrievePetDescription() {
+        VirtualPet underTest = new VirtualPet("Sam", "Norwegian Elkhound");
+        assertEquals("Norwegian Elkhound", underTest.getVirtualPetDescriptor());
+    }
+
+    @Test
+    public void virtualPetShouldGiveWater() {
+        VirtualPet undertest = new VirtualPet("Sam", "Norwegian Elkhound");
+        int thirstLevel = undertest.giveWater(15);
+        assertEquals(0, thirstLevel);
+    }
+
+    @Test
+    public void virtualPetShouldGiveFood() {
+        VirtualPet undertest = new VirtualPet("Sam", "Norwegian Elkhound");
+        int hungerLevel = undertest.giveFood(25);
+        assertEquals(10, hungerLevel);
+    }
+
+    @Test
+    public void virtualPetShouldPlayWithPet() {
+        VirtualPet undertest = new VirtualPet("Sam", "Norwegian Elkhound");
+        int boredomLevel = undertest.playWithPet(25);
+        assertEquals(0, boredomLevel);
+    }
+
+    @Test
+    public void tickShouldMakeHungerThirstBoredomGoUp() {
+        VirtualPet undertest = new VirtualPet("Sam", "Norwegian Elkhound");
+        undertest.tick();
+        assertEquals(20,undertest.getHungerLevel());
+        assertEquals(20,undertest.getThirstLevel());
+        assertEquals(20,undertest.getBoredomLevel());
+    }
 }
